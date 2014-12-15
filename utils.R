c7d37d1d13472f7d1ed00e8b9f4442a56507fcb6
Mikhail Dozmorov <mikhail.dozmorov@gmail.com>
2014-12-14 21:13:12 -0500

    Milestone: adding unified function showHeatmap that plots Cell x Factor heatmap and barplots

--- utils.R
+++ utils.R
@@ -1,4 +1,20 @@
-# Convert a mantrix of raw p-values (with "-" indicating depletion) into -log10-transformed
+# Set up the environment
+library(dplyr)
+library(plyr)
+library(reshape2)
+library(cluster)
+library(ggplot2)
+library(gplots)
+# Work paths
+# trackDb <- tbl_df(read.table("/Users/mikhail/Documents/Work/GenomeRunner/genomerunner_database/hg19/gf_descriptions_hg19.txt", sep="\t", header=F))
+# gfAnnot <- tbl_df(read.table("/Users/mikhail/Documents/Work/GenomeRunner/genomerunner_database/hg19/GFs_hg19_joined_cell_factor.txt", sep="\t", header=F))
+# Home paths
+trackDb <- tbl_df(read.table("/Users/mikhaildozmorov/Documents/Work/GenomeRunner/genomerunner_database/hg19/gf_descriptions_hg19.txt", sep="\t", header=F))
+gfAnnot <- tbl_df(read.table("/Users/mikhaildozmorov/Documents/Work/GenomeRunner/genomerunner_database/hg19/GFs_hg19_joined_cell_factor.txt", sep="\t", header=F))
+
+
+## ----------------------------------------------------------------------------------
+## Convert a matrix of raw p-values (with "-" indicating depletion) into -log10-transformed
 mtx.transform<-function(x){
   tmp<- -log10(abs(x)) # -log10 transformation without sign
   for (i in 1:nrow(x)){
@@ -9,13 +25,240 @@ mtx.transform<-function(x){
   return(tmp)
 }
 
-# Correct for multiple testing the matrix of transformed p-values
-mtx.adjust<-function(x){ 
-  tmp<- -log10(apply(1/10^abs(x), 2, p.adjust)) # Adjust absolute p-values for multiple testing and convert them back to -log10
+## ----------------------------------------------------------------------------------
+## Convert a matrix of -log10-transformed p-values (with "-" indicating depletion) into raw linear scale
+mtx.untransform<-function(x){
+  tmp<- 1/10^abs(x) # -log10 transformation without sign
   for (i in 1:nrow(x)){
     for (j in 1:ncol(x)){
-      if (x[i,j]<0) {tmp[i,j]<- -tmp[i,j]} # Add sign, if neede
+      if (x[i, j]<0) {tmp[i, j]<- -tmp[i,j]} # Add sign, if needed
     }
   }
   return(tmp)
+}
+
+## ----------------------------------------------------------------------------------
+## Correct for multiple testing the matrix of transformed p-values
+mtx.adjust.log10<-function(x, method="fdr"){ 
+  tmp<- -log10(apply(1/10^abs(x), 2, p.adjust, method=method)) # Adjust absolute p-values for multiple testing and convert them back to -log10
+  for (i in 1:nrow(x)){
+    for (j in 1:ncol(x)){
+      if (x[i,j]<0) {tmp[i,j]<- -1*tmp[i,j]} # Add sign, if needed
+    }
+  }
+  return(tmp)
+}
+
+## ----------------------------------------------------------------------------------
+## Adjust raw p-values for mutliple testing
+mtx.adjust.raw <- function(x, adjust="fdr") {
+  tmp <- x; # Keep signs in the original version
+  tmp.adj <- apply(abs(x), 2, p.adjust, method=adjust) 
+  for (i in 1:nrow(x)){
+    for (j in 1:ncol(x)){
+      if (tmp[i, j] < 0) tmp.adj[i, j] <- -1*tmp.adj[i, j] # Add sign, if needed
+    }
+  }
+  return(tmp.adj)
+}
+
+## ----------------------------------------------------------------------------------
+## Adjust ONE vector of p-values for mutliple testing, return -log10-transformed values
+mtx.adjust.1 <- function(x, adjust="fdr", isLog10=TRUE) { 
+  if (isLog10) {
+    tmp <- -log10(p.adjust(1/10^abs(x), method=adjust))
+  } else {
+    tmp <- -log10(p.adjust(abs(x), method=adjust))
+  }
+  for (i in 1:length(x)) {
+    if (x[i] < 0) tmp[i] <- -1*tmp[i] # Add a "-" sign, if it was there previously  
+  }
+  return(tmp)
+}
+
+## ----------------------------------------------------------------------------------
+## Filters non-significant rows from a matrix
+mtx.filter <- function(x, pval=0.05){
+  idx <- apply(x, 1, function(i) {sum(abs(i) < pval) >= 1})
+  tmp <- as.matrix(x[idx, , drop=F])
+  colnames(tmp) <- colnames(x)
+  #rownames(tmp) <- rownames(x)[idx]
+  idxEnrich <- as.logical(apply(tmp, 1, function(x) {length(x[x > 0 & x < pval]) > 0}))
+  tmp1 <- as.matrix(tmp[idxEnrich, , drop=F])
+  colnames(tmp1) <- colnames(tmp)
+  #rownames(tmp1) <- rownames(tmp)[idxEnrich]
+  idxDeplet <- as.logical(apply(tmp, 1, function(x) {length(x[x < 0 & -x < pval]) > 0}))
+  tmp2 <- as.matrix(tmp[idxDeplet, , drop=F])
+  colnames(tmp2) <- colnames(tmp)
+  #rownames(tmp2) <- rownames(tmp)[idxDeplet]
+  if (nrow(tmp1) > 1) tmp1 <- tmp1[order(apply(tmp1, 1, function(x) {mean(x[x > 0 & x < pval])}), decreasing=F), , drop=F]
+  if (nrow(tmp2) > 1) tmp2 <- tmp2[order(apply(tmp2, 1, function(x) {mean(x[x < 0 & -x < pval])}), decreasing=T), , drop=F]
+  return(list(tmp1 <- data.frame(gfs=rownames(tmp1), tmp1), 
+              tmp2 <- data.frame(gfs=rownames(tmp2), tmp2)))
+}
+
+## ----------------------------------------------------------------------------------
+## Shows top 10 enriched and depleted associations as tables
+##
+## Parameters:
+##     fname - full path to enrichment matrix
+##     isLog10 - whether a matrix is log10-transformed (TRUE for GR, FALSE for GR WEB)
+##     adjust - whether to correct p-values for multiple testing ("fdr" for GR, "none" for GR WEB)
+##     pval - cutoff of calling an enrichment significant
+##
+showTable <- function(fname, isLog10=TRUE, adjust="fdr", pval=0.05) {
+  mtx <- read.table(fname, sep="\t", row.names=1, header=T)
+  if (isLog10) {mtx <- mtx.untransform(mtx)} # Un- -log10 transform p-values, if needed
+  mtx <- mtx.adjust.raw(mtx, adjust) # Adjust for multiple testing
+  mtx <- mtx.filter(mtx, pval) # Get two sepatate lists, enchched and depleted
+  numEnrich <- nrow(mtx[[1]]) # Total number of significantly enriched
+  numDeplet <- nrow(mtx[[2]]) # and depleted associations
+  if (numEnrich > 0) {
+    print(paste("The total number of significantly ENRICHED associations is:", numEnrich))
+    if (numEnrich > 10) numEnrich <- 10
+    #mtx.enrich <- merge(data.frame(pval=format(as.matrix(mtx[[1]][1:numEnrich, , drop=F]), digits=3, scientific=T),
+    #                               row.names=rownames(as.matrix(mtx[[1]]))[1:numEnrich]), trackDb.hg19, by="row.names", all.x=T, sort=T)
+    #mtx.enrich <- data.frame(pval=format(as.matrix(mtx[[1]][1:numEnrich, , drop=F]), digits=3, scientific=T), row.names=rownames(as.matrix(mtx[[1]]))[1:numEnrich])
+    mtx.enrich <- left_join(mtx[[1]], trackDb.hg19, by=c("gfs" = "V1"))
+    pander(mtx.enrich[1:numEnrich, ], split.table=Inf)
+    mtx[[1]] <- as.data.frame(mtx[[1]]); rownames(mtx[[1]]) <- mtx[[1]][, 1]; mtx[[1]] <- mtx[[1]] <- mtx[[1]][, -1, drop=F]
+    mtx[[1]][mtx[[1]] == 0] <- .Machine$double.xmin
+    barplot1(mtx.transform(mtx[[1]][1:numEnrich, , drop=F]), 15)
+    #    grid.table(mtx.enrich, gp=gpar(fontsize=6))
+    print("---------------------------------------------------------------")
+  }
+  if (numDeplet > 0) {
+    print(paste("The total number of significantly DEPLETED associations is:", numDeplet))
+    if (numDeplet > 10) numDeplet <- 10
+    mtx.deplet <- merge(data.frame(pval=format(as.matrix(mtx[[2]][1:numDeplet, , drop=F]), digits=3, scientific=T), row.names=rownames(as.matrix(mtx[[2]]))[1:numDeplet]), trackDb.hg19, by="row.names", all.x=T)
+    #mtx.deplet <- data.frame(pval=format(as.matrix(mtx[[2]][1:numDeplet, , drop=F]), digits=3, scientific=T), row.names=rownames(as.matrix(mtx[[2]]))[1:numDeplet])
+    mtx.deplet <- left_join(mtx[[2]], trackDb.hg19, by=c("gfs" = "V1"))
+    pander(mtx.deplet[1:numDeplet, ], split.table=Inf)
+    mtx[[2]] <- as.data.frame(mtx[[2]]); rownames(mtx[[2]]) <- mtx[[2]][, 1]; mtx[[2]] <- mtx[[2]] <- mtx[[2]][, -1, drop=F]
+    mtx[[2]][mtx[[2]] == 0] <- .Machine$double.xmin
+    barplot1(mtx.transform(mtx[[2]][1:numDeplet, , drop=F]), 15)
+    #grid.table(mtx)
+    print("---------------------------------------------------------------")
+  }
+}
+
+## ----------------------------------------------------------------------------------
+## Make a barplot of -log10-transformed p-values matrix.
+## 
+## Parameters:
+##     mtx - matrix of -log10-transformed p-values
+##     location - where to put the legend (e.g., "topright", "bottomright")
+##     bottom - bottom margin (e.g., 5 or 15)
+##     names.args - names of bar groups
+## 
+barplot1<-function(mtx, location="topright", bottom=5, names.args){
+  par(mar=c(bottom,5,2,2)+0.1)
+  groupcolors<-rainbow(ncol(mtx)) #c("yellow2","steelblue3","steelblue3","springgreen)
+  b<-barplot(as.matrix(t(mtx)), beside=T,  ylab="-log10(p-value)\nnegative = underrepresentation", col=groupcolors,space=c(0.2,1), cex.names=0.7, las=2, names.arg=names.args) # ,legend.text=colnames(mtx),args.legend=list(x=7,y=4))
+  #lines(c(0,100),c(-log10(0.01),-log10(0.01)),type="l",lty="dashed",lwd=2)
+  #lines(c(0,100),c(log10(0.01),log10(0.01)),type="l",lty="dashed",lwd=2)
+  legend(location, legend=colnames(mtx), fill=groupcolors, cex=0.5)
+ }
+
+
+## ----------------------------------------------------------------------------------
+## Creates Cell x Factor heatmap from a Histone/TFBS one-column matrix of enrichments.
+## Also, plots barplots of most enriched/depleted associations
+##
+## Parameters:
+##     fname - full path to enrichment matrix
+##     colnum - which column(s) to use, in case of multi-column matrix. If a single column
+## selected, and the factor is set, a heatmap of Cell x Factor enrichments is plotted
+##     factor - subset matrix by "Histone"/"Tfbs" enrichments, or "none". If "none", 
+## barplot is labeled with table names, otherwise, by "cell.factor" labels
+##     isLog10 - whether a matrix is log10-transformed (TRUE for GR, FALSE for GR WEB)
+##     adjust - whether to correct p-values for multiple testing ("fdr" for GR, "none" for GR WEB)
+##     pval - cutoff of calling an enrichment significant
+##     numofsig - number of cells in each row/column with non-missing significant values
+##     fileName - save the reshaped wide matrix into a file
+##
+## Examples:
+##     For the original GR:
+## showHeatmap("matrix.txt", colnum=1, factor="Histone", isLog10=TRUE, adjust="fdr", pval=0.1, numofsig=4, fileName=NULL)
+##     For the GR WEB:
+## showHeatmap("matrix.txt", colnum=1, factor="Tfbs", isLog10=FALSE, adjust="none", pval=0.1, numofsig=4, fileName=NULL) - will plot the heatmap and the barplot
+## showHeatmap("matrix.txt", colnum=c(1, 5), factor="none", isLog10=FALSE, adjust="none", pval=0.1, numofsig=1, fileName=NULL) - will plot the barplot only
+
+
+showHeatmap <- function(fname, colnum=1, factor="none", isLog10=TRUE, adjust="fdr", pval=0.1, numofsig=4, fileName=NULL) {
+  mtx <- tbl_df(read.table(fname, sep="\t", fill=T, header=F, stringsAsFactors=F)) # No header and row.names
+  cols <- mtx[1, ] # Keep header
+  if (factor != "none") {
+    mtx <- mtx[grep(factor, mtx$V1), c(1, colnum + 1)] # Subset by factor and column
+  } else {
+    mtx <- mtx[-1, c(1, colnum + 1)] # Do not subset
+  }
+   # Adjust for multiple testing and -log10 transform, if needed
+  for (i in 1:length(colnum)) {
+    mtx[, i + 1] <- mtx.adjust.1(as.numeric(unlist(mtx[, i + 1])), adjust=adjust, isLog10=isLog10)
+  i}
+  # Join with annotations
+  mtx <- left_join(mtx, gfAnnot[, c(1, 3, 5, 2)], by=c("V1" = "V1")) 
+  colnames(mtx) <- c("GF", make.names(cols[colnum]), "cell", "factor", "description") # Rename columns
+  
+  ## Creates Cell x Factor heatmap from a matrix of enrichments from a Histone/Tfbs matrix.
+  if (length(colnum) == 1 & factor != "none") { # If only 1 column selected, we can plot heatmap
+    # Make wide matrix
+    mtx.cast <- dcast(mtx, formula=cell~factor, fun.aggregate=mean, value.var=make.names(cols[colnum]))
+    rownames(mtx.cast) <- mtx.cast$cell # Reassign rownames
+    mtx.cast <- mtx.cast[, -1] # Remove no longer needed first column
+    # Filter it
+    dim(mtx.cast) # Check original dimensions
+    # What remains if we remove rows/cols with nothing significant
+    dim(mtx.cast[apply(mtx.cast, 1, function(x) sum(abs(x) > -log10(pval), na.rm=T)) >= numofsig, apply(mtx.cast, 2, function(x) sum(abs(x) > -log10(pval), na.rm=T))>=numofsig])
+    # Trim the matrix
+    mtx.cast<-mtx.cast[apply(mtx.cast, 1, function(x) sum(abs(x) > -log10(pval), na.rm=T)) >= numofsig, apply(mtx.cast, 2, function(x) sum(abs(x) > -log10(pval), na.rm=T)) >= numofsig] 
+    if (nrow(mtx.cast) == 0 | ncol(mtx.cast) == 0) { 
+      print("Nothing significant, cannot plot heatmap")
+      return()
+    } else {
+      # Plotting
+      par(cex.main=0.65, oma=c(5,0,0,5), mar=c(5, 4.1, 4.1, 5)) # Adjust margins
+      color<-colorRampPalette(c("blue", "yellow", "red")) # Define color gradient
+      dist.method<-"euclidean"  
+      hclust.method<-"ward.D2"
+      h<-heatmap.2(as.matrix(mtx.cast), trace="none", density.info="none", col=color, distfun=function(x){dist(x, method=dist.method)}, hclustfun=function(x){hclust(x, method=hclust.method)}, cexRow=1, cexCol=1)
+      if (!is.null(fileName)) {
+        write.table(mtx.cast[h$rowInd, h$colInd], fileName, sep="\t", col.names=NA)
+      }
+    }    
+  } 
+  
+  if (TRUE) { # If more than 1 column, we can also plot barplots
+    if (!is.null(fileName)) { # Save the matrix, if needed
+      write.table(mtx, fileName, sep="\t", col.names=NA, row.names=F)
+    }
+    mtx <- as.data.frame(mtx) # Make data frame, to allow row names
+    rownames(mtx) <- mtx$GF; mtx <- mtx[, -1] # Make row names
+    mtx.sorted.up <- list(); mtx.sorted.dn <- list() # Storage for sorted matrixes 
+    for (i in 1:length(colnum)) {
+      # For each column, reorder p-values and store top X most significantly enriched
+      mtx.sorted.up[[length(mtx.sorted.up) + 1]] <- mtx[order(mtx[, i], decreasing=T)[1:round(15/length(colnum))], ]
+      # And depleted
+      mtx.sorted.dn[[length(mtx.sorted.dn) + 1]] <- mtx[order(mtx[, i], decreasing=F)[1:round(15/length(colnum))], ]
+    }
+    # Combine lists into matrixes
+    mtx.barplot.up <- ldply(mtx.sorted.up, rbind)
+    mtx.barplot.dn <- ldply(mtx.sorted.dn, rbind)
+    # Create barplot names
+    if (factor == "none") {
+      names.args.up <- make.names(unlist(lapply(mtx.sorted.up, rownames)), unique=T)
+      names.args.dn <- make.names(unlist(lapply(mtx.sorted.dn, rownames)), unique=T)
+      bottom <- 15
+    } else {
+      names.args.up <- paste(mtx.barplot.up$cell, mtx.barplot.up$factor, sep=".")
+      names.args.dn <- paste(mtx.barplot.dn$cell, mtx.barplot.dn$factor, sep=".")
+      bottom <- 5
+    }
+    # Plot barplots
+    barplot1(mtx.barplot.up[, seq(1:length(colnum)), drop=F], "topright", bottom=bottom, names.args=names.args.up)
+    print("---------------------------------------------------------------")
+    barplot1(mtx.barplot.dn[, seq(1:length(colnum)), drop=F], "bottomright", bottom=bottom, names.args=names.args.dn)
+    print("---------------------------------------------------------------")
+  }
 }
