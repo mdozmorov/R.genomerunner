c6edb7a427eafd14cf7e20e5816fdf06a5c21439
Lukas <lks_cara@yahoo.com>
2015-06-16 10:28:19 -0500

    Dendrogram working.  Testing tabs

--- server.R
+++ server.R
@@ -13,22 +13,32 @@ shinyServer(function(input, output) {
     rcorr(as.matrix(mtx), type=input$cmbCorType)[[1]]
   })
   
-  get.hclust.mat <- reactive({
+  get.hclust.dendrogram <- reactive({
     cor.mat <- get.corr.matrix() 
-    hclust(as.dist(1-cor.mat), method=input$cmbClustMethod);
-  })
-  
-  get.dend.file <- reactive({
-    readRDS(file = "/home/lukas/heatmap.dend.rds")
+    as.dendrogram(hclust(as.dist(1-cor.mat), method=input$cmbClustMethod))
   })
   
   output$heatmap <- renderD3heatmap({     
     cor.mat <- get.corr.matrix()
-    hclustergram <- get.hclust.mat()
-    d3heatmap::d3heatmap(as.matrix(cor.mat),heatmap_options = list(Rowv=hclustergram$Rowv,Colv=hclustergram$Colv,keep.dendro=TRUE))
+    hclustergram <- get.hclust.dendrogram()
+    d3heatmap::d3heatmap(as.matrix(cor.mat),heatmap_options = list(Rowv=hclustergram,Colv=hclustergram,keep.dendro=TRUE))
   })
-  output$pltDend <- renderPlot({
-    dend = get.dend.file()
-    plot(dend)
+  
+  output$pltDend <- renderPlot({ 
+    cor.mat <- get.corr.matrix() # this line ensure that dendrogram is redrawn when heatmap is
+    hclustergram <- get.hclust.dendrogram() # ensures that dendrogram is redrawn when hclust method is changed
+    
+    dend = readRDS(file = "/home/lukas/heatmap.dend.rds")
+    plot(as.dendrogram(dend, hang=-1)) # Plot dendrogram
+    cl_num <- input$sldNumClust # Empirically set desired numter of clusters
+    cols <- rainbow(cl_num) # Make colors for clusters
+    rect.hclust(as.hclust(dend), k=cl_num, border=cols) # Define the clusters by rectangles
+    
+    # what's the purpose of this code?
+    #abline(h=hcut)
+    #as.hclust(dend)$height %>% density %>% plot
+    #abline(v=hcut)
+    
+    #mtx.clust <- dend %>% mtx.clusters(height=hcut, minmembers=3)
   })
 })
--- ui.R
+++ ui.R
@@ -1,31 +1,41 @@
 library(shiny)
 library(d3heatmap)
 
-shinyUI(fluidPage(
-  titlePanel("GRsnp HeatMap"),
-  
-  sidebarLayout(
-    sidebarPanel(
-      selectInput("cmbHeatmap", label = "Select which matrix to visualize", 
-                  choices = list("P-value" = "/home/lukas/gftest/matrix_PVAL.txt", 
-                                 "Odds Ratio" = "/home/lukas/gftest/matrix_OR.txt")),
-      selectInput('cmbCorType',label = "Correlation coefficient type",
-                  choices = list("Pearson's" = "pearson",
-                                 "Spearman's" = "spearman")),
-      selectInput("cmbClustMethod",label = "Clustering method (hclust)", 
-                  choices = list("ward.D",
-                                 "ward.D2",
-                                 "single",
-                                 "complete",
-                                 "average",
-                                 "mcquitty",
-                                 "median",
-                                 "centroid") ),
-      sliderInput("sldNumGroups","Number of clusters",min = 0,max=10,value = 3)
-      
-    ),
-    
-    mainPanel(d3heatmapOutput("heatmap", width = "100%", height = "400px"),
-              plotOutput("pltDend"))
+shinyUI(
+  tabsetPanel(
+    tabPanel("Heatmap",
+      fluidPage(
+        fluidRow(
+          column(4,
+                 selectInput("cmbHeatmap", label = "Select which matrix to visualize", 
+                             choices = list("P-value" = "/home/lukas/gftest/matrix_PVAL.txt", 
+                                            "Odds Ratio" = "/home/lukas/gftest/matrix_OR.txt"))),
+          column(4,
+                 selectInput('cmbCorType',label = "Correlation coefficient type",
+                             choices = list("Pearson's" = "pearson",
+                                            "Spearman's" = "spearman"))
+          ),
+          
+          column(4,
+                 selectInput("cmbClustMethod",label = "Clustering method (hclust)", 
+                             choices = list("ward.D",
+                                            "ward.D2",
+                                            "single",
+                                            "complete",
+                                            "average",
+                                            "mcquitty",
+                                            "median",
+                                            "centroid")
+                 )
+          )),
+        d3heatmapOutput("heatmap", width = "100%", height = "400px"),
+        fluidRow(
+          column(12,
+                 sliderInput("sldNumClust","Number of clusters",min = 2,max=10,value = 3))
+        ),
+        plotOutput("pltDend",width = "100%", height = "500px")
+      )),
+    tabPanel("Epigenetic results",
+             fluidPage(h3("test")))
+    )
   )
-))
