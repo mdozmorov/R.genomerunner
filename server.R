b99f73dca08ca63bd404a03173677ca22c0c3ad9
Mikhail Dozmorov <mikhail.dozmorov@gmail.com>
2015-09-04 17:15:44 -0400

    Moved epigenetic code so that it is run when controls are loaded

--- server.R
+++ server.R
@@ -402,14 +402,22 @@ shinyServer(function(input, output,session) {
       
       cat("Running vis", file="~/logs/shiny.log")
     }, message = "Calculating regulatory differences",value = 1.0)
+    mtx.deg
+  })
+  
+  output$tblEpigenetics <-renderDataTable({
+    mtx <- get.matrix()
+    cat("Table, made it", file="~/logs/shiny.log")
+    validate(need(ncol(mtx)>2,"Need at least 3 SNPs of interest files to perform clustering."))
+    validate(need(nrow(mtx)>4,"Need at 5 least genome features to perform clustering."))
+    #validate(need(try(get.epigenetics.table()),"Either nothing is significant, or there are too few SNP sets per cluster. Re-run the analysis using more SNP sets, or try a different clustering method."))
+    mtx.deg <- get.epigenetics.table()
     selectedCor = input$cmbEpigenetics
     cat(input$cmbEpigenetics, file="~/logs/shiny.log")
-    # check if any results were returned. Note this will always be the case the first time the tabe is loaded
-    # as this code is run twice by Shiny due to updateSelectInput being a lazy evaluation
-    if (is.null(names(mtx.deg)) | selectedCor == "Results not ready yet."){ 
-      return("Results not ready yet.")
+    #validate(need(table.epi != "Results not ready yet.", "Results not ready yet."))
+    if (mtx.deg == "Results lot ready yet." | length(mtx.deg) == 0){
+	return(data.frame(NoResult="There is nothing signficant to show"))
     }
-       
   
     #convert values to numeric form for sorting purposes
     if(input$cmbMatrix == "matrix_PVAL.txt"){
@@ -420,22 +428,9 @@ shinyServer(function(input, output,session) {
         mtx.deg[[selectedCor]][["adj.p.val"]] <- scientific_format(3)(as.numeric(mtx.deg[[selectedCor]][["adj.p.val"]]))
     }
     colnames(mtx.deg[[selectedCor]])[1] <- "epigenomic_feature"
-    mtx.deg[[selectedCor]][, !(colnames(mtx.deg[[1]]) %in% c("full_path", "URL", "full_description", "category", "category_desc"))]
-  })
-  
-  output$tblEpigenetics <-renderDataTable({
-    mtx <- get.matrix()
-    cat("Table, made it", file="~/logs/shiny.log")
-    validate(need(ncol(mtx)>2,"Need at least 3 SNPs of interest files to perform clustering."))
-    validate(need(nrow(mtx)>4,"Need at 5 least genome features to perform clustering."))
-    #validate(need(try(get.epigenetics.table()),"Either nothing is significant, or there are too few SNP sets per cluster. Re-run the analysis using more SNP sets, or try a different clustering method."))
-    table.epi <- get.epigenetics.table()
-    #validate(need(table.epi != "Results not ready yet.", "Results not ready yet."))
-    if (table.epi == "Results not ready yet." | length(table.epi) == 0){
-	return(data.frame(NoResult="There is nothing signficant to show"))
-    }
+    table.epi <- mtx.deg[[selectedCor]][, !(colnames(mtx.deg[[1]]) %in% c("full_path", "URL", "full_description", "category", "category_desc"))]
     num.char <- 50
-    table.epi <- apply( table.epi,c(1,2),function(x) {
+    table.epi<- apply( table.epi,c(1,2),function(x) {
       if (!is.na(x) & nchar(x)>num.char){
         return(paste(substring(x,1,num.char),  "..."))
       } else{
@@ -443,9 +438,8 @@ shinyServer(function(input, output,session) {
       }
     
     })
-    cat(table.epi[[1]],file="~/logs/shiny.log")
     cat("Table2, made it", file="~/logs/shiny.log")
-    table.epi
+    DT::datatable(table.epi)
   },options = list( lengthMenu = list(c(10, 50, 100,-1), c('10', '50','100', 'All')),
                     pageLength = 10))
   
