0850fcbc077ee71c1350ffa010f6bf02b24da371
Mikhail Dozmorov <mikhail.dozmorov@gmail.com>
2015-12-31 15:33:39 -0500

    Update GF descriptions

--- gfAnnot.R
+++ gfAnnot.R
@@ -1,4 +1,7 @@
 gfAnnot <- read.table("data/gf_descriptions.txt", sep = "\t", header = TRUE, as.is = TRUE)
-saveRDS("data/gfAnnot.Rds")
 
-gfAnnot <- readRDS("data/gfAnnot.Rds")
+save(list="gfAnnot", file = "data/gfAnnot.rda")
+load("/Users/mikhail/Documents/Work/GenomeRunner/MDmisc/data/gfAnnot.rda")
+
+# saveRDS(gfAnnot, file = "data/gfAnnot.Rds")
+# gfAnnot <- readRDS("data/gfAnnot.Rds")
