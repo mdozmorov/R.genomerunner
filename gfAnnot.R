656a0e4796fc2eb9cb839a14b0211cffc0c85bc8
Mikhail Dozmorov <mikhail.dozmorov@gmail.com>
2016-02-25 10:34:45 -0500

    Automating annotation for db_6

--- gfAnnot.R
+++ gfAnnot.R
@@ -1,3 +1,19 @@
+library(dplyr)
+# Read in gf_descriptions file created by gr-dbcreator 
+gf_descriptions_nodups_hg19 <- read.table("data/db_6.00_02-07-2016/grsnp_db/hg19/gf_descriptions_nodups_hg19.txt.gz", sep = "\t", header = T, stringsAsFactors = F)
+# Sort it
+gf_descriptions_nodups_hg19 <- gf_descriptions_nodups_hg19[order(gf_descriptions_nodups_hg19[, "category"],
+                                                                 gf_descriptions_nodups_hg19[, "cell"],
+                                                                 gf_descriptions_nodups_hg19[, "factor"]), ]
+# Available ENCODE cells
+gf_ENCODE_cells <- gf_descriptions_nodups_hg19$cell[ grep("^E", gf_descriptions_nodups_hg19$cell, invert = T)] %>% unique
+# Read in ENCODE cell types
+ENCODE_cells <- read.table("data/ENCODE_cells.txt", sep = "\t", header = T, quote = "\"", fill = T, stringsAsFactors = F)
+tail(sort(table(ENCODE_cells$cell)))
+setdiff(toupper(gf_ENCODE_cells), toupper(ENCODE_cells$cell))
+
+
+# Convert descriptions to RDA object
 gfAnnot <- read.table("data/gf_descriptions.txt", sep = "\t", header = TRUE, as.is = TRUE)
 
 save(list="gfAnnot", file = "data/gfAnnot.rda")
